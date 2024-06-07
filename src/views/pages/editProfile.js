580206048c06700e00c233a9b602ee1646313ab0
to-ma-to-es <136566012+to-ma-to-es@users.noreply.github.com>
2024-06-07 14:30:18 +0800

    9th commit - edit profile complete

--- src/views/pages/editProfile.js
+++ src/views/pages/editProfile.js
@@ -47,34 +47,49 @@ class EditProfileView {
   render(){
     const template = html`
       <va-app-header title="Edit Profile" user=${JSON.stringify(Auth.currentUser)}></va-app-header>
-      <div class="page-content">        
+      <div class="page-content edit-page">        
         ${(this.user == null) ? html`
           <sl-spinner></sl-spinner>
         `:html`
-          <p>Updated: ${moment(Auth.currentUser.updatedAt).format('MMMM Do YYYY, @ h:mm a')}</p>
+         <!-- <p>Updated: ${moment(Auth.currentUser.updatedAt).format('MMMM Do YYYY, @ h:mm a')}</p> -->
           <form class="page-form input-validation-required" @submit=${this.updateProfileSubmitHandler.bind(this)}>
-            <div class="input-group">
-              <sl-input type="text" name="firstName" value="${this.user.firstName}" placeholder="First Name"></sl-input>
+            <div class="edit-header">
+              <h1>UPDATE ACCOUNT</h1>
+              <sl-icon  @click=${()=> gotoRoute('/profile')} class="x-lg" name="x-lg"></sl-icon>
+            </div>   
+            
+            <div class="edit-flex">
+              <div class="edit-section-one">
+                <div class="input-group">
+                  <sl-input label="Change First Name" class="edit-profile-input" type="text" name="firstName" value="${this.user.firstName}" placeholder="First Name"></sl-input>
+                </div>
+                <div class="input-group">
+                  <sl-input label="Change Last Name" class="edit-profile-input" type="text" name="lastName" value="${this.user.lastName}" placeholder="Last Name"></sl-input>
+                </div>
+                <div class="input-group">
+                  <sl-input label="Change Email" class="edit-profile-input" type="text" name="email" value="${this.user.email}" placeholder="Email Address"></sl-input>
+                </div>  
+              </div>    
+              <div class="edit-section-two"> 
+                <div class="input-group">
+                  <sl-textarea class="edit-profile-textarea" label="Change Description" name="bio" rows="4" placeholder="Add bio here" value="${this.user.bio}"></sl-textarea>
+                </div>     
+                <div class="input-group" class="avatar-change">
+                  <label class="avatar-label">Change Avatar</label><br>          
+                  ${(this.user.avatar) ? html`
+                    <sl-avatar image="${App.apiBase}/images/${this.user.avatar}"></sl-avatar>
+                    <input type="file" name="avatar" />
+                  `: html`
+                    <input type="file" name="avatar" />
+                  `}
+                </div>
+              </div> 
             </div>
-            <div class="input-group">
-              <sl-input type="text" name="lastName" value="${this.user.lastName}" placeholder="Last Name"></sl-input>
-            </div>
-            <div class="input-group">
-              <sl-input type="text" name="email" value="${this.user.email}" placeholder="Email Address"></sl-input>
-            </div>       
-            <div class="input-group">
-              <sl-textarea name="bio" rows="4" placeholder="Add bio here" value="${this.user.bio}"></sl-textarea>
-            </div>     
-            <div class="input-group">
-              <label>Avatar</label><br>          
-              ${(this.user.avatar) ? html`
-                <sl-avatar image="${App.apiBase}/images/${this.user.avatar}"></sl-avatar>
-                <input type="file" name="avatar" />
-              `: html`
-                <input type="file" name="avatar" />
-              `}
-            </div>
-            <sl-button variant="primary" type="submit" class="submit-btn">Update Profile</sl-button>
+                <div class="button-wrapper">
+                  <sl-button variant="primary" type="submit" class="submit-btn account-submit-btn" >Update Account</sl-button>
+                </div>
+              
+            
           </form>
         `}
       </div>
